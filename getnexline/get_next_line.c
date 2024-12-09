6373616c31ad069894e4af4868e55cc5de5b7fdc
Selami Ali Şimşek <sesimsek@k1m25s09.42istanbul.com.tr>
2024-12-09 17:56:24 +0300

    update

--- getnexline/get_next_line.c
+++ getnexline/get_next_line.c
@@ -6,13 +6,25 @@
 /*   By: sesimsek <sesimsek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 17:08:21 by sesimsek          #+#    #+#             */
-/*   Updated: 2024/12/07 21:26:18 by sesimsek         ###   ########.fr       */
+/*   Updated: 2024/12/08 22:42:37 by sesimsek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+#include "get_next_line.h"
 
 char	*get_next_line(int fd)
 {
-	
+	char *str;
+	char *line;
+	line = malloc(1);
+	str = malloc(5);
+	while(ft_strchr(str,'\n') == NULL)
+	{
+		int a = read(fd,str,4);
+		if ( a == 0)
+			return(NULL);
+		str[a] = '\0';
+		line = ft_strjoin(line,str);
+	}
+	return(line);	
 }
