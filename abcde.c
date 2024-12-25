70fbcd7fdb6390a64c4a772f9fa4958866cc1938
Selami Ali Şimşek <sesimsek@k1m11s06.42istanbul.com.tr>
2024-12-25 15:08:37 +0300

    ada

--- abcde.c
+++ abcde.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: sesimsek <sesimsek@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/06 17:08:21 by sesimsek          #+#    #+#             */
-/*   Updated: 2024/12/25 15:07:35 by sesimsek         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
 
 char *get_line(char *str)
@@ -117,21 +105,3 @@ char *get_next_line(int fd)
     str = update_str(str);
     return (line);
 }
-
-// char	*get_next_line(int fd)
-// {
-// 	char *str;
-// 	char *line;
-// 	line = malloc(1);
-// 	str = malloc(5);
-// 	// while(ft_strchr(str,'\n') == NULL)
-// 	// {
-// 		int a = read(fd,str,4);
-// 		printf("%d", a);
-// 		// if ( a == 0)
-// 		// 	return(NULL);
-// 		str[a] = '\0';
-// 		line = ft_strjoin(line,str);
-// 	// }
-// 	return(line);	
-// }
