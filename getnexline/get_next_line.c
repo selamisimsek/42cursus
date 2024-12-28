74aee450e8fa7b9a5e197408ab17139659e682c8
selamisimsek <102020695+selamisimsek@users.noreply.github.com>
2024-12-28 14:14:41 +0000

    update

--- getnexline/get_next_line.c
+++ getnexline/get_next_line.c
@@ -12,109 +12,95 @@
 
 #include "get_next_line.h"
 
-char *rwfd(int fd, char *readstr)
+char	*rwfd(int fd, char *readstr)
 {
-    int rb;
-    char *str;
-    char *temp;
+	int		rb;
+	char	*str;
+	char	*temp;
 
-    str = malloc(BUFFER_SIZE + 1);
-    if (!str)
-        return (NULL);
-    rb = 1;
-    while (!ft_strchr(readstr, '\n') && rb != 0)
-    {
-        rb = read(fd, str, BUFFER_SIZE);
-        if (rb == -1)
-            return (free(str),free(readstr), NULL);
-        str[rb] = '\0';
-        temp = readstr;
-        readstr = ft_strjoin(readstr, str);
-        free(temp);
-    }
-    free(str);
-    return (readstr);
+	str = malloc(BUFFER_SIZE + 1);
+	if (!str)
+		return (NULL);
+	rb = 1;
+	while (!ft_strchr(readstr, '\n') && rb != 0)
+	{
+		rb = read(fd, str, BUFFER_SIZE);
+		if (rb == -1)
+			return (free(str), free(readstr), NULL);
+		str[rb] = '\0';
+		temp = readstr;
+		readstr = ft_strjoin(temp, str);
+		free(temp);
+	}
+	free(str);
+	return (readstr);
 }
 
-char *update_str(char *str1)
+char	*update_str(char *str1)
 {
-    char *str;
-    int i = 0;
-    int j = 0;
+	char	*str;
+	int		i;
+	int		j;
 
-    while (str1[i] != '\n' && str1[i])
-        i++;
-    if (!str1[i])
-    {
-        free(str1);
-        return (NULL);
-    }
-    str = malloc(ft_strlen(str1) - i + 1);
-    if (!str)
-        return (NULL);
-    i++;
-    while (str1[i])
-        str[j++] = str1[i++];
-    str[j] = '\0';
-    free(str1);
-    return (str);
+	i = 0;
+	j = 0;
+	while (str1[i] != '\n' && str1[i])
+		i++;
+	if (!str1[i])
+	{
+		free(str1);
+		return (NULL);
+	}
+	str = malloc(ft_strlen(str1) - i + 1);
+	if (!str)
+		return (NULL);
+	i++;
+	while (str1[i])
+		str[j++] = str1[i++];
+	str[j] = '\0';
+	free(str1);
+	return (str);
 }
 
-char *oneline(char *str)
+char	*oneline(char *str)
 {
-    char *line;
-    int i = 0;
+	char	*line;
+	int		i;
 
-    while (str[i] != '\n' && str[i])
-        i++;
-    if (str[i] == '\n' && str[i])
-        line = malloc(i + 2);
-    else
-        line = malloc(i + 1);
-    if (!line)
-        return (NULL);
-    i = 0;
-    while (str[i] && str[i] != '\n')
-    {
-        line[i] = str[i];
-        i++;
-    }
-    if (str[i] == '\n')
-        line[i++] = '\n';
-    line[i] = '\0';
-    return (line);
+	i = 0;
+	if (!str[0])
+		return (NULL);
+	while (str[i] != '\n' && str[i])
+		i++;
+	if (str[i] == '\n' && str[i])
+		line = malloc(i + 2);
+	else
+		line = malloc(i + 1);
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (str[i] && str[i] != '\n')
+	{
+		line[i] = str[i];
+		i++;
+	}
+	if (str[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    static char *readstr;
-    char *line;
+	static char	*readstr;
+	char		*line;
 
-    if (!readstr)
-        readstr = ft_strdup("");
-    readstr = rwfd(fd, readstr);
-    if (!readstr)
-        return (NULL);
-    line = oneline(readstr);
-    readstr = update_str(readstr);
-    return (line);
+	if (!readstr)
+		readstr = ft_strdup("");
+	readstr = rwfd(fd, readstr);
+	if (!readstr)
+		return (NULL);
+	line = oneline(readstr);
+	readstr = update_str(readstr);
+	return (line);
 }
-
-// char	*get_next_line(int fd)
-// {
-// 	char *str;
-// 	char *line;
-// 	line = malloc(1);
-// 	str = malloc(5);
-// 	int a = 1;
-// 	 while(a != 0)
-// 	{
-// 		a = read(fd,str,4);
-// 		// printf("%d", a);
-// 		if ( a == 0)
-// 			return(NULL);
-// 		str[a] = '\0';
-// 		line = ft_strjoin(line,str);
-// 	}
-// 	return(line);
-// }
