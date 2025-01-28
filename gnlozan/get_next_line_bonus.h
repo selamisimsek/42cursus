3df8b7f71a5561a16f120ee680b5602ef116669b
Selami Ali Şimşek <sesimsek@k1m09s03.42istanbul.com.tr>
2025-01-28 22:08:52 +0300

    ozangnl

--- gnlozan/get_next_line_bonus.h
+++ gnlozan/get_next_line_bonus.h
@@ -3,27 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sesimsek <sesimsek@student.42.fr>          +#+  +:+       +#+        */
+/*   By: oboran <oboran@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/28 17:29:35 by sesimsek          #+#    #+#             */
-/*   Updated: 2024/12/28 19:44:01 by sesimsek         ###   ########.fr       */
+/*   Created: 2025/01/23 19:05:51 by oboran            #+#    #+#             */
+/*   Updated: 2025/01/23 19:07:23 by oboran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
+# define BUFFER_SIZE 42
+# include "stdlib.h"
+# include "sys/types.h"
+# include "sys/uio.h"
+# include "unistd.h"
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 4
-# endif
-
-# include <stdlib.h>
-# include <unistd.h>
-
+char	*get_next_line_(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strchr(const char *src, int c);
-char	*ft_strdup(const char *s1);
-size_t	ft_strlen(const char *str);
-char	*get_next_line(int fd);
+char	*ft_strchar(const char *string, int wantedchar);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
+size_t	ft_strlen(const char *string);
 
 #endif
---
+++ gnlozan/get_next_line_utils_bonus.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oboran <oboran@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/23 19:05:53 by oboran            #+#    #+#             */
+/*   Updated: 2025/01/23 19:07:48 by oboran           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char   *ft_strjoin(char const *s1, char const *s2)
+{
+	int		totalsize;
+	char	*res;
+	int		i;
+	int		j;
+
+	i = 0;
+	totalsize = ft_strlen(s1) + ft_strlen(s2);
+	res = malloc(sizeof(char) * (totalsize + 1));
+	if (!res || !s1 || !s2)
+		return (NULL);
+	while (s1[i] != 0)
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != 0)
+	{
+		res[i] = s2[j];
+		i++;
+		j++;
+	}
+	res[totalsize] = 0;
+	return (res);
+}
+
+char	*ft_strchar(const char *string, int wantedchar)
+{
+	char	*str;
+
+	str = (char *)string;
+	while (*str != wantedchar && *str != 0)
+		str++;
+	if(*str == wantedchar)
+		return (str);
+	else
+		return (NULL);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	char *str;
+	size_t i;
+
+	str = (char *)s;
+	i = 0;
+	while (i < n)
+	{
+		str[i] = '\0';
+		i++;
+	}
+}
+
+void *ft_calloc(size_t count, size_t size)
+{
+	char *res;
+
+	res = malloc(size * count);
+	if (!res)
+		return (NULL);
+	ft_bzero(res, size * count);
+	return (res);
+}
+
+size_t ft_strlen(const char *string)
+{
+	int	i;
+
+	i = 0;
+	while (string[i])
+		i++;
+	return (i);
+}
